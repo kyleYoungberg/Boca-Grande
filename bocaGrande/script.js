7ab9d4b456c3c10f28312792b560cbcbf238b6cc
kyleYoungberg <49645346+kyleYoungberg@users.noreply.github.com>
2019-04-15 08:52:22 -0400

    Add files via upload

---
+++ bocaGrande/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function($){
+    $('.slideshowContainer').slick({
+        slidesToShow: 1,
+        SlidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+    
+   
+});
+
